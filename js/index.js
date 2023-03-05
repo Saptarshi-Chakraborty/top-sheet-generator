66cd930052c1d69543d600574d916cc6cee9fab7
Saptarshi Chakraborty <saptarshichakraborty04@outlook.com>
2023-03-05 18:34:46 +0530

    1st Production Version

---
+++ js/index.js
@@ -0,0 +1,137 @@
+console.log("%cIndex.js active", "color:yellow;font-size:18px;");
+
+let loadingInterval;
+
+let f = document.getElementById("form-ca2");
+let modalLaunchBtn = document.getElementById("launchModalBtn");
+let downloadAgainLink = document.getElementById("dAgainButton");
+let studentNameField = document.getElementById("sName");
+let titleField = document.getElementById("title");
+let cusromNameField = document.getElementById("cName");
+let makautRollField = document.getElementById('makautRoll');
+let classRollPart1Field = document.getElementById('cRoll1');
+let classRollPart2Field = document.getElementById('cRoll2');
+let classRollPart3Field = document.getElementById('cRoll3');
+let displayRollField = document.getElementById('displayRoll');
+
+
+f.addEventListener("submit", () => {
+    event.preventDefault();
+    toggleLoading();
+
+    // Version 19 on Nov 21, 6:39 PM
+    const API = "https://script.google.com/macros/s/AKfycbzZXoda0jWeVzNfBMr-qrtU09Afh_6rKHm4m9DgVENESzay9AXLctvVdpvTlwP94vj1/exec";
+
+    let mRoll = "";
+    let sName = document.getElementById("sName").value.trim();
+    let sRoll = updateRoll();
+    let title = document.getElementById("title").value.trim();
+    let subject = document.getElementById("subjectCode").value.trim();
+    let sem = document.getElementById("sem").value.trim();
+    let cName = cusromNameField.value.trim();
+
+
+    // Set FILE Name in mRoll Field
+    if (cName === "") {
+        mRoll = sRoll;
+    } else {
+        if (String(cName).length <= 3) {
+            alert("Please give a valid name");
+            toggleLoading();
+            return;
+        }
+        else
+            mRoll = cName;
+    }
+
+
+    // Prepare parameters for fetch
+    let primaryFormData = { mRoll, sName, sRoll, title, subject, sem, cName: "null" };
+    let formData = new FormData();
+    for (let key in primaryFormData)
+        formData.append(key, primaryFormData[key]);
+
+    let params = { method: "POST", body: formData };
+
+    // make http request through fetch
+    fetch(API, params).then(res => res.text()).then((_data) => {
+        let res;
+        try {
+            res = JSON.parse(_data);
+        } catch (error) {
+            alert("An unexpected error occured. Conact Admin");
+        }
+        console.log(res);
+
+        let status = res.status;
+
+        if (status == "success") {
+            let downloadUrl = res.url;
+            showSuccessModal(downloadUrl);
+
+            displayRollField.value = '22-CSE-';
+            cusromNameField.placeholder = "Default filename = Your Roll Number";
+
+            f.reset();
+        } else {
+            alert("An unexpected error occured. Contact admin");
+        }
+
+        toggleLoading();
+    }).catch((error) => {
+        toggleLoading();
+        alert("An unexpected error occurred during connecting to the Server");
+        console.log(`ERROR Fetch : ${error}`);
+    });
+
+});
+
+classRollPart1Field.addEventListener("change", updateRoll);
+classRollPart2Field.addEventListener("change", updateRoll);
+classRollPart3Field.addEventListener("input", updateRoll);
+
+
+
+//  Secondary Functions
+
+function toggleLoading() {
+    let submitBtn = document.getElementById("submitBtn");
+    if (submitBtn.innerText === "SUBMIT") {
+        submitBtn.innerText = "Loading..";
+        loadingInterval = setInterval(() => {
+            submitBtn.innerText += ".";
+        }, 1700);
+        submitBtn.setAttribute("disabled", true);
+    } else {
+        if (loadingInterval !== undefined)
+            clearInterval(loadingInterval);
+        submitBtn.innerText = "SUBMIT";
+        submitBtn.removeAttribute("disabled");
+    }
+}
+
+
+function showSuccessModal(downloadUrl) {
+    window.location.replace(downloadUrl);
+
+    downloadAgainLink.href = downloadUrl;
+    modalLaunchBtn.click();
+}
+
+
+function updateRoll() {
+    let part1 = String(classRollPart1Field.value).trim();
+    let part2 = String(classRollPart2Field.value).trim();
+    let part3 = String(classRollPart3Field.value).trim();
+
+    let classRoll = `${part1}-${part2}-${part3}`;
+    classRoll = String(classRoll).toUpperCase();
+
+    displayRollField.innerText = classRoll;
+    cusromNameField.placeholder = `Default filename = ${classRoll}`;
+
+    return classRoll;
+}
+
+
+
---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("%c Script.js active", "color:yellow,font-size:20px");
+
+nextUrl("SB");
