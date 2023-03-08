13609f21e9d4f89264ddafe3449e6989e8b803ff
Saptarshi Chakraborty <saptarshichakraborty04@outlook.com>
2023-03-08 22:39:55 +0530

    New Update for 2nd Sem - Production

--- js/index.js
+++ js/index.js
@@ -8,22 +8,24 @@ let downloadAgainLink = document.getElementById("dAgainButton");
 let studentNameField = document.getElementById("sName");
 let titleField = document.getElementById("title");
 let cusromNameField = document.getElementById("cName");
-let makautRollField = document.getElementById('makautRoll');
+let makautRollField = document.getElementById('mRoll');
 let classRollPart1Field = document.getElementById('cRoll1');
 let classRollPart2Field = document.getElementById('cRoll2');
 let classRollPart3Field = document.getElementById('cRoll3');
 let displayRollField = document.getElementById('displayRoll');
 
+// fillTestingValues();
 
-f.addEventListener("submit", () => {
+f.addEventListener("submit", function () {
+    console.log("Form Submit Button Clicked");
     event.preventDefault();
     toggleLoading();
 
-    // Version 19 on Nov 21, 6:39 PM
-    const API = "https://script.google.com/macros/s/AKfycbzZXoda0jWeVzNfBMr-qrtU09Afh_6rKHm4m9DgVENESzay9AXLctvVdpvTlwP94vj1/exec";
-
-    let mRoll = "";
-    let sName = document.getElementById("sName").value.trim();
+    // Version 5 on Mar 8, 10:30 PM
+    const API = "https://script.google.com/macros/s/AKfycbwbOMO3PuiOpQCKra95q3cHLJKp3R1AxuUsQTaCnOJjOTgtk4Vj32i-HRB6ItYhW6kQKg/exec";
+    
+    let mRoll = makautRollField.value.trim();
+    let sName = studentNameField.value.trim();
     let sRoll = updateRoll();
     let title = document.getElementById("title").value.trim();
     let subject = document.getElementById("subjectCode").value.trim();
@@ -32,21 +34,25 @@ f.addEventListener("submit", () => {
 
 
     // Set FILE Name in mRoll Field
-    if (cName === "") {
-        mRoll = sRoll;
+    if (cName === '') {
+        cName = "null";
+    }
+    else if (cName === "null") {
+        alert(`File name can't be "null" `);
+        toggleLoading();
+        return;
     } else {
         if (String(cName).length <= 3) {
             alert("Please give a valid name");
             toggleLoading();
             return;
         }
-        else
-            mRoll = cName;
     }
 
 
+
     // Prepare parameters for fetch
-    let primaryFormData = { mRoll, sName, sRoll, title, subject, sem, cName: "null" };
+    let primaryFormData = { mRoll, sName, sRoll, title, subject, sem, cName };
     let formData = new FormData();
     for (let key in primaryFormData)
         formData.append(key, primaryFormData[key]);
@@ -70,7 +76,7 @@ f.addEventListener("submit", () => {
             showSuccessModal(downloadUrl);
 
             displayRollField.value = '22-CSE-';
-            cusromNameField.placeholder = "Default filename = Your Roll Number";
+            cusromNameField.placeholder = "Default filename = Your MAKAUT Roll";
 
             f.reset();
         } else {
@@ -100,7 +106,7 @@ function toggleLoading() {
         submitBtn.innerText = "Loading..";
         loadingInterval = setInterval(() => {
             submitBtn.innerText += ".";
-        }, 1700);
+        }, 2000);
         submitBtn.setAttribute("disabled", true);
     } else {
         if (loadingInterval !== undefined)
@@ -128,10 +134,20 @@ function updateRoll() {
     classRoll = String(classRoll).toUpperCase();
 
     displayRollField.innerText = classRoll;
-    cusromNameField.placeholder = `Default filename = ${classRoll}`;
+    // cusromNameField.placeholder = `Default filename = ${classRoll}`;
 
     return classRoll;
 }
 
+
+
+
+// Fill Some Random values in the form for testing purpose
+function fillTestingValues() {
+    studentNameField.value = "Jhon HELLO";
+    makautRollField.value = "148003897";
+    classRollPart3Field.value = "068";
+    titleField.value = "This is a Demo Title"
+}
 
 
